f4cb2a26cfd6c1ab2e934fa57cf8da0101a7440f
Quinci135 <quinci135@gmail.com>
2020-09-10 13:23:20 -0700

    Initial Commit

---
+++ PluginTemplate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace TestPlugin
+{
+    [ApiVersion(2, 1)]
+    public class TestPlugin : TerrariaPlugin
+    {
+        public override string Author => "Quinci";
+
+        public override string Description => "A sample test plugin";
+
+        public override string Name => "Test Plugin";
+
+        public override Version Version => new Version(1, 0, 0, 0);
+
+        public TestPlugin(Main game) : base(game)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Deregister hooks here
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
